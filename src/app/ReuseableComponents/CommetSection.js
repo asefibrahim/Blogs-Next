e8c52c9061ddf268571370ba93f5d74894d06c35
John Doe <mohaiminur24@gmail.com>
2023-08-14 18:19:36 +0600

    get blog comment and design ui is complete

--- src/app/ReuseableComponents/CommetSection.js
+++ src/app/ReuseableComponents/CommetSection.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import StoreComment from "../customHook/StoreComment";
+import GetComment from "../customHook/GetComment";
+import { BiSolidUserCircle } from "react-icons/bi";
 
-const CommetSection = ({id}) => {
-    const submitcomment = (event)=>{
-        event.preventDefault();
-        const form = event.target;
-        const comment = form.comment.value;
-        const user = form.user.value;
-        if(!comment && !user){
-            Swal.fire(
-                'Empty?',
-                'Must filup from first',
-                'question'
-              )
-            return;
-        }
-        const commentdata = {comment,user};
-        StoreComment(id,commentdata);
-        form.reset();
+const CommetSection = ({ id }) => {
+  const [comments, setComments] = useState(null);
+  useEffect(() => {
+    const comments = GetComment(id);
+    setComments(comments);
+  }, [id]);
+  const submitcomment = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const comment = form.comment.value;
+    const user = form.user.value;
+    if (!comment && !user) {
+      Swal.fire("Empty?", "Must filup from first", "question");
+      return;
     }
+    const commentdata = { comment, user };
+    StoreComment(id, commentdata);
+    form.reset();
+    setComments([...comments,commentdata ]);
+  };
   return (
     <div className="py-10 mt-5 border-t">
       <form onSubmit={submitcomment}>
@@ -42,6 +46,16 @@ const CommetSection = ({id}) => {
           className="text-sm px-5 py-2 bg-yellow-500 mt-3 rounded-md font-bold text-white"
         />
       </form>
+      <div className="my-3">
+        <h1 className="font-serif">All comment is here ..................</h1>
+        {comments &&
+          comments.map((comment) => {
+            return <div className="text-sm my-1 shadow-sm py-2 px-5">
+                <h1 className="font-bold text-base"><BiSolidUserCircle className="inline-block mr-2"/>{comment.user}</h1>
+                <p className="text-gray-500">{comment.comment}</p>
+            </div>;
+          })}
+      </div>
     </div>
   );
 };
