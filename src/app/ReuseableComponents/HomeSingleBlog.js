512fd00b6b324ae8c5c0ff9ee3f3abfa4d5b7e8a
John Doe <mohaiminur24@gmail.com>
2023-08-14 11:47:19 +0600

    responsive home page blog page design is complete

---
+++ src/app/ReuseableComponents/HomeSingleBlog.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+
+const HomeSingleBlog = (Blog) => {
+    const {urlToImage,title,publishedAt,description,source} = Blog.Blog;
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2 my-5 shadow-md p-5 gap-2 rounded-md'>
+            <img className='w-4/5 rounded-sm' src={urlToImage} alt="" />
+            <div className='flex flex-col'>
+                <h1 className='font-bold text-3xl'>{source.name}</h1>
+                <h1 className='text-lg'>{title}</h1>
+                <p className='text-sm text-gray-500'>{publishedAt}</p>
+                <p className='my-3 text-sm text-gray-500'>{description}</p>
+                <Link className='mt-auto px-5 py-2 bg-slate-600 text-center text-white rounded-md shadow-md hover:bg-slate-700' href={"/"}>Click here for Details</Link>
+            </div>
+        </div>
+    );
+};
+
+export default HomeSingleBlog;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
+"use client"
 
+import { useEffect, useState } from "react"
+import HomeSingleBlog from "./ReuseableComponents/HomeSingleBlog";
 
 export default function Home() {
+  const [Blog, setBlog] = useState(null);
+  useEffect(()=>{
+    fetch("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=8e26901aaa0943c5bc1f82180ef4449b").then(res=> res.json())
+    .then(data=> setBlog(data.articles));
+  },[]);
+
   return (
     <div>
-      <h1>This is home page</h1>
+      {
+        Blog && Blog.map((singleBlog,index)=> <HomeSingleBlog key={index}  Blog={singleBlog}/>)
+      }
     </div>
   )
 }
